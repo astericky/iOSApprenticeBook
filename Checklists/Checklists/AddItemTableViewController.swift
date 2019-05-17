4db293ae2d216a81677f43164fd2b4669e9a1299
Chris Sanders <astericky@gmail.com>
2019-05-16 18:42:50 -0700

    update add item view controller

--- Checklists/Checklists/AddItemTableViewController.swift
+++ Checklists/Checklists/AddItemTableViewController.swift
@@ -9,13 +9,16 @@
 import UIKit
 
 class AddItemTableViewController: UITableViewController {
+  // MARK: - Outlets
+  @IBOutlet weak var textField: UITextField!
   
-  // MARK:- Actions
+  // MARK: - Actions
   @IBAction func cancel() {
     navigationController?.popViewController(animated: true)
   }
   
   @IBAction func done() {
+    print("Contents of the text field: \(textField.text!)")
     navigationController?.popViewController(animated: true)
   }
   
@@ -25,7 +28,10 @@ class AddItemTableViewController: UITableViewController {
     navigationItem.largeTitleDisplayMode = .never
   }
   
-
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    textField.becomeFirstResponder()
+  }
   
   // MARK: - Table View Delegates
   override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
